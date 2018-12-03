8bec6287d514e192eb3ab07db19cab0d933e88ee
Ilias Naamane <ilias.naamane1@gmail.com>
2018-12-03 02:51:56 +0100

    add AES and remove DES + initialization vector + padding / unpadding

--- client.py
+++ client.py
@@ -1,12 +1,26 @@
 import socket
+from binascii import hexlify, unhexlify
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import DES
+from Crypto.Cipher import DES, AES
+from Crypto import Random
 
+# Establish connection to the server
 s = socket.socket()
-s.connect(("localhost",7777))
+s.connect(("localhost",8787))
+print("connection ok")
 s.sendall("connexion ok")
 
+# Block size for AES
+BS=16
+aesKey = '2b7e151628aed2a6abf7158809cf4f3c'
 
+# http://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
+
+# Padding to be multiple of BS
+def pkcs5_pad(s):
+    BLOCK_SIZE = 16
+    return s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+# RSA encryption
 def encryptRSA(server_string,string):
 	server_string = server_string.replace("public key:", '')
 	server_string = server_string.replace("\r\n", '')
@@ -15,41 +29,41 @@ def encryptRSA(server_string,string):
 	encrypted = server_public_key.encrypt(string, 32)
 	return encrypted
 
+# generate Initialization vector for AES
+def generateInitVector():
+	return Random.new().read(AES.block_size)
 
-def encryptDES(desKey, msg):
-	des =  DES.new(desKey)
-	if len(msg) % 8 != 0:
-	    toAdd = 8 - len(msg) % 8
-	print toAdd
-	for i in range(0,toAdd):
-		msg = msg+" "
-	print msg
-	cipher_text = des.encrypt(msg)
-	return cipher_text
-
-
-
+# AES encryption
+def encryptAES(aesKey, iv, msg):
+	msg = pkcs5_pad(msg)
+	cipher = AES.new(unhexlify(aesKey), AES.MODE_CBC, unhexlify(iv))
+	return hexlify(cipher.encrypt(msg))
 
 #Receive public key string from server
-server_string = s.recv(1024)
-#Encrypt message and send to server
-
-desKey = '01234567'
-encryptedRSA = encryptRSA(server_string,desKey)
-print "Encrypted RSA: "+str(encryptedRSA)
+serverString = s.recv(1024)
+# encrypt aes key using RSA public key received from the server
+encryptedRSA = encryptRSA(serverString,aesKey)
+print "encryptedRSA="+str(encryptedRSA)
+# send aesKey encrypted with RSA public key to the server
 s.sendall("encryptedRSA="+str(encryptedRSA))
+# open input to the user
 message = raw_input()
-encryptedDES = encryptDES(desKey,message)
-print "Encrypted Message: "+encryptedDES
-s.sendall("encryptedMessage="+encryptedDES)
+# generate initialization vector in hexadecimal format and send it to the server
+iv = hexlify(generateInitVector())
+print "Init Vector="+iv
+s.sendall("Init Vector="+iv)
+# encrypt the message using AES and send it to the server
+encryptedAES = encryptAES(aesKey,iv,message)
+print "Encrypted Message="+encryptedAES
+s.sendall("encryptedMessage="+str(encryptedAES))
 
-#Server's response
+#Server's response if Server OK the server decrypt the message
 server_response = s.recv(1024)
 server_response = server_response.replace("\r\n", '')
 if server_response == "Server: OK":
     print "Le serveur a decrypte le message"
 
 #Tell server to finish connection
-s.sendall("Quit")
+#s.sendall("Quit")
 print(s.recv(1024)) #Quit server response
 s.close()
--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 from socket import *
 from Crypto.PublicKey import RSA
 from Crypto import Random
-from Crypto.Cipher import DES
-
+from Crypto.Cipher import AES
+from binascii import hexlify, unhexlify
+## Generate RSA private & public keys
 random_generator = Random.new().read
 private_key = RSA.generate(1024, random_generator)
 public_key = private_key.publickey()
 
+## Server Binding
 HOST = "localhost" #localhost
-PORT = 7777
+PORT = 8787
 s = socket(AF_INET, SOCK_STREAM)
 s.bind((HOST, PORT))
-s.listen(4)
+s.listen(6)
 conn, addr = s.accept()
 print "Server is running"
 
+# Unpadding for AES decryption message
+def pkcs5_unpad(s):
+    s = s[0:-ord(s[-1])]
+    return s
+
+# decrypt data using RSA private key
 def decryptRSA(data,private_key):
 	print "decryptRSA....\n"
 	
@@ -24,28 +32,39 @@ def decryptRSA(data,private_key):
 	decrypted = private_key.decrypt(encrypted)
 	return decrypted
 
-def decryptDES(desKey,data):
-	print "decryptDES.....\n"
-	data = data.replace("encryptedMessage=",'')
-	des =  DES.new(desKey)
-	return des.decrypt(data)
+# encrypt data using RSA private key
+def encryptRSA(data,string):
+	#Convert string to key
+	server_public_key = RSA.importKey(server_string)
+	encrypted = server_public_key.encrypt(string, 32)
+	return encrypted
 
-
+#decrypt message using AES
+def decryptAES(aesKey, iv, ciphertext):
+	cipher = AES.new(unhexlify(aesKey), AES.MODE_CBC, unhexlify(iv))
+	print "ciphertext="+unhexlify("ef531f5d790bcf1d72e1ab109076f177")
+	return pkcs5_unpad(cipher.decrypt(unhexlify(ciphertext)))
 
 while True:
+	# First message from the client which normally indicates that connection is ok
 	data = conn.recv(1024)
 	data = data.replace("\r\n", '')
 	if data == "connexion ok":
 		conn.send("public key:"+public_key.exportKey()+"\n")
-
+	# Decrypt aesKey using RSA private key
 	elif "encryptedRSA" in data:
-		desKey= decryptRSA(data,private_key)
+		aesKey= decryptRSA(data,private_key)
 		conn.send("Server: OK")
-		print "Decrypted des key = " + desKey
+		print "Decrypted aes key = " + aesKey
+	# Receive initialization vector from the client
+	elif "Init Vector" in data:
+		iv = data.replace("Init Vector=",'')
+		print "Init Vector="+iv
+	# Receive encrypted message and decrypt it using aes key ( Previously decrypt it by RSA)
 	elif "encryptedMessage" in data:
-		print "Encrypted Message : "+data
-		decrypted = decryptDES(desKey,data)
-		print "Decrypted message = "+ decrypted
+		data = data.replace("encryptedMessage=","")
+		decrypted = decryptAES(aesKey,iv,data)
+		print "Decrypted message="+ decrypted
 	elif data == "Quit": break
 
 #Stop server
